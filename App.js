890e9b0c66750950cf57cda3392124be755b86a3
seque <sequeirajosecarlos@gmail.com>
2023-09-20 11:07:19 -0600

    Creacion de carpeta components junto a sus componentes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Form from './components/form';
+import User from './components/user'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>StreamNow</Text>
+      <Text>Mira lo que quieras</Text>
       <StatusBar style="auto" />
     </View>
   );
